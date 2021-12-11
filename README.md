2ca39755bdc28211887c6397cb5dac1d712a3ae6
ashleytkim <57121181+ashleytkim@users.noreply.github.com>
2021-12-10 19:15:00 -0500

    added mario.mid

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# voice-filters
+# song/album mood generator, markov to create new melody
+
+https://github.com/Tonejs/Midi
+
+
+Have 4 songs and 4 albums pre uploaded. Also add instructions for how a user can add more. 
+
+1. read in MIDI file and convert to note sequence 
+2. user sets markov n (either 1, 2, 3)
+3. markov nth order (everything will be done on notes)
+- create transition matrix
+- calculate probability disribution 
+- define first note as the first note of song 
+- follow transitions 
--- filter.js
+++ filter.js
@@ -1,5 +1,43 @@
+import { Midi } from '@tonejs/midi'
+const midi = new Midi() 
+
 const audioCtx = new(window.AudioContext || window.webkitAudioContext);
 
+// Song 
+const marioSelected = document.getElementById('Mario')
+const otherSelected = document.getElementById('Other1')
+
+var songSelected; 
+var noteSequence; 
+
+
+// MIDI to Note Sequence 
+
+
+if (marioSelected.checked){
+    MIDItoNoteSequence('/mario.mid')
+}else if(otherSelected.checked){
+    otherSelected = 
+}
+
+function MIDItoNoteSequence(songPath){
+    // convert Blob containing MIDI to a note sequence 
+    noteSequence = blobToNoteSequence()
+
+}
+
+function nOrder(){
+    
+}
+function generateTransitions(n){
+
+}
+
+function calculateNextNote(noteSequence){
+
+}
+
+/*
 if (navigator.mediaDevices) {
     navigator.mediaDevices.getUserMedia({ "audio": true }).then((stream) => {
         const microphone = audioCtx.createMediaStreamSource(stream);
@@ -16,4 +54,5 @@ if (navigator.mediaDevices) {
     console.log("ERROR: Unable to access media devices.");
     // browser unable to access media devices
     // (update your browser)
-}
+}*/
+
