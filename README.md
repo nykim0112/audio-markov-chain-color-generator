6057800d403aca6d543f2dd11733d58f02b8bd4d
ashleytkim <57121181+ashleytkim@users.noreply.github.com>
2021-12-11 14:56:22 -0500

    transition matrix added

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ https://github.com/Tonejs/Midi
 Have 4 songs and 4 albums pre uploaded. Also add instructions for how a user can add more. 
 
 1. read in MIDI file and convert to note sequence 
-2. user sets markov n (either 1, 2, 3)
+2. user sets markov n (either 1, 2)
 3. markov nth order (everything will be done on notes)
-- create transition matrix
+- create transition matrix (prior notes.pitch and notes.duration)
 - calculate probability disribution 
 - define first note as the first note of song 
 - follow transitions 
--- filter.js
+++ filter.js
@@ -1,7 +1,3 @@
-import { Midi } from '@tonejs/midi'
-const midi = new Midi() 
-
-const audioCtx = new(window.AudioContext || window.webkitAudioContext);
 
 // Song 
 const marioSelected = document.getElementById('Mario')
@@ -10,6 +6,25 @@ const otherSelected = document.getElementById('Other1')
 var songSelected; 
 var noteSequence; 
 
+TWINKLE_TWINKLE = {
+    notes: [
+      {pitch: 60, startTime: 0.0, endTime: 0.5},
+      {pitch: 60, startTime: 0.5, endTime: 1.0},
+      {pitch: 65, startTime: 1.0, endTime: 1.5},
+      {pitch: 67, startTime: 1.5, endTime: 2.0},
+      {pitch: 69, startTime: 2.0, endTime: 2.5},
+      {pitch: 69, startTime: 2.5, endTime: 3.0},
+      {pitch: 67, startTime: 3.0, endTime: 4.0},
+      {pitch: 65, startTime: 4.0, endTime: 4.5},
+      {pitch: 65, startTime: 4.5, endTime: 5.0},
+      {pitch: 64, startTime: 5.0, endTime: 5.5},
+      {pitch: 60, startTime: 5.5, endTime: 6.0},
+      {pitch: 65, startTime: 6.0, endTime: 6.5},
+      {pitch: 67, startTime: 6.5, endTime: 7.0},
+      {pitch: 60, startTime: 7.0, endTime: 8.0},  
+    ],
+    totalTime: 8
+  };
 
 // MIDI to Note Sequence 
 
@@ -17,25 +32,102 @@ var noteSequence;
 if (marioSelected.checked){
     MIDItoNoteSequence('/mario.mid')
 }else if(otherSelected.checked){
-    otherSelected = 
+    // otherSelected = 
+}else{
+    songPath = TWINKLE_TWINKLE
 }
 
 function MIDItoNoteSequence(songPath){
     // convert Blob containing MIDI to a note sequence 
-    noteSequence = blobToNoteSequence()
 
 }
 
-function nOrder(){
+
+function generateTransitions(noteSequence){
+    var n = document.getElementById("nOrder").value
+
+    var uniquePitches = TWINKLE_TWINKLE.notes.map(item => item.pitch).filter((value, index, self) => self.indexOf(value) === index)
+    // for each unique pitch in song, determine transitions 
+    var matrix = new Array([]); 
+    var pitchCols = new Array()
+    var pitchRows = new Array()
+
+    console.log(noteSequence.notes.length)
+    // for each note in the sequence 
+    for(var i=n; i<noteSequence.notes.length; i++){
+        var prev2 = noteSequence.notes[i-2]
+        var prev1 = noteSequence.notes[i-1]
+        var curr = noteSequence.notes[i]
+        var currIndex; // corresponds to col
+        var prevIndex; // corresponds to row
     
-}
-function generateTransitions(n){
+
+        // if pitch not in matrix, add pitch to list
+        if(pitchCols.includes(curr.pitch) == false){
+            pitchCols.push(curr.pitch)
+            console.log("MAKING NEW COL")
+        }
+
+        currIndex = pitchCols.indexOf(curr.pitch)
+
+        // determine if prev combo is in matrix
+        if(pitchRows.includes(prev2.pitch + ","+ prev1.pitch)){
+            // both exist, so add 1 to that element 
+            console.log("PREV2 EXISTS")
+            prevIndex = pitchRows.indexOf(prev2.pitch + ","+ prev1.pitch)
+            matrix[prevIndex][currIndex] = matrix[prevIndex][currIndex] + 1
+        }else{
+            // prev doesnt exist yet, make new row 
+            console.log("MAKING NEW ROW")
+            matrix.push([])
+            pitchRows.push(prev2.pitch + ","+ prev1.pitch)
+            prevIndex = pitchRows.indexOf(prev2.pitch + ","+ prev1.pitch)
+            matrix[prevIndex][currIndex] = 1
+        }
+
+        console.log("curr index and prev are " + currIndex + prevIndex)
+        console.log("current is " + JSON.stringify(curr))
+        console.log("pitch rows are  " + JSON.stringify(pitchRows))
+        console.log("pitch cols are  " + JSON.stringify(pitchCols))
+        console.table(matrix)
+
+    }
+
+    console.log(JSON.stringify(matrix[0]))
+    // then normalize matrix so rows sum to 1 
+    for(var i=0; i<pitchRows.length; i++){
+        var rowSum = matrix[i].reduce(add, 0)
+
+        // then divide each col by rowSum 
+        for(var j=0; j<pitchCols.length; j++){
+            console.log("reducing and " + matrix[i][j]/rowSum)
+            matrix[i][j] = matrix[i][j]/rowSum
+        }
+    }
+    console.log('NEW MATRIX')
+    console.table(matrix)
 
 }
+
+function add(accumulator, a) {
+    return accumulator + a;
+  }
 
 function calculateNextNote(noteSequence){
 
 }
+
+function playMarkov(song){
+    var transition_matrix = generateTransitions(song)
+}
+
+const playButton = document.querySelector('button');
+playButton.addEventListener('click', function() {
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)
+    
+    generateTransitions(TWINKLE_TWINKLE); 
+
+}, false);
 
 /*
 if (navigator.mediaDevices) {
